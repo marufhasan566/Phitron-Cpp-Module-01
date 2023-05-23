1adeb5c5868733e08c5e00980e2c7831b4c85e79
marufhasan566 <marufhasan.iitju47@gmail.com>
2023-05-23 23:25:54 +0600

    input output

---
+++ input_output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // std::cout << "Hello Asus\n";
+    // std::cout << "Hello Maruf" << std::endl;
+    // std::cout << "Hello Bro" << std::endl;
+    // std::cout << "Hello" << std::endl
+    //           << "World" << std::endl;
+    // std::cout << "Hello "
+    //           << "World";
+    int m = 69, n = 70;
+    cout << m << " " << n;
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello Maruf\n");
+    return 0;
+}
