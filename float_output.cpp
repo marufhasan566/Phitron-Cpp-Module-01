cee4be8d1d045d80c670dbc9df24caa8608cfc7a
marufhasan566 <marufhasan.iitju47@gmail.com>
2023-05-24 08:20:59 +0600

    float output

--- float_output.cpp
+++ float_output.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
-
-    int a, b;
+    float a, b;
     cin >> a >> b;
-    cout << a << endl
+    cout << fixed << setprecision(2) << a << endl
          << b;
     return 0;
-} // namespace std;
+}
